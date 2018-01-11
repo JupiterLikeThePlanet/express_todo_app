0e654c00d06ddaf53f863dff68f1889617215268
Jupiter <jupiterbaudot@gmail.com>
2018-01-11 14:16:42 -0800

    Trash some files

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
 var express = require('express');
 var app = express();
+var PORT = 3000;
 
-var PORT = 3000
+var middleware = {
+	requireAuthentication: function(req, res, next) {
+		console.log("Hey now. This is private.");
+	}
+}
+
+// app.use(middleware.requireAuthentication());
 
 app.get('/', function(req, res) {
 	res.send("Hellooooo Nurse");
 });
 
+app.get('/about', function(req, res) {
+	res.send("Abottobad");
+});
 
-
-// app.listen(PORT, "Your server is now up and running on port " + PORT);
+app.listen(PORT, function(){
+	console.log("Your server is now up and running on port " + PORT);
+});
 
 // var express = require('express');
 
-app.listen(3000);
+// app.listen(PORT);
 
 
 

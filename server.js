c81133044e335664fd122f4a2836868b97cf0d0e
Jupiter <jupiterbaudot@gmail.com>
2018-01-12 15:02:49 -0800

    Start from scratch

--- server.js
+++ server.js
@@ -1,24 +1,15 @@
 var express = require('express');
+
 var app = express();
-//heroku gives us a port we can listen to
+
 var PORT = process.env.PORT || 3000;
-var middleware = require("./middleware.js")
 
 
 
-//Application level
-app.use(middleware.logger);
-//important to specify middleware up top. Won't run after /about route
-// app.use(middleware.requireAuthentication);
-
-//route level middleware (e.g. something that requires user to be logged in)
-app.get('/about', middleware.requireAuthentication, function(req, res) {
-	res.send("About A Boy by Nick Hornby");
-});
-
-app.use(express.static(__dirname + "/public"));
-
+app.get("/", function(req, res){
+    res.send("We are not in Kansas anymore, TODO");
+})
 
 app.listen(PORT, function(){
-	console.log("Your server is now up and running on port " + PORT);
-});
+    console.log("We are always listening at " + PORT)
+})

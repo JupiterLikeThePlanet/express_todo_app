aaf82fba9d4653afcb6c616f386eaa50a7f99d64
Jupiter <jupiterbaudot@gmail.com>
2018-01-15 17:12:09 -0800

    Correct res.json(matchedTodo) from res.json.matchedTodo

--- server.js
+++ server.js
@@ -4,12 +4,56 @@ var app = express();
 
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+    id: 1,
+    description: 'Call Shelly',
+    completed: false,
+}, {
+    id: 2,
+    description: 'Eat Taco Bell',
+    completed: false,
+}, {
+    id: 3,
+    description: 'Complain',
+    completed: true,
+}];
 
+//GET todos
 
 app.get("/", function(req, res){
     res.send("We are not in Kansas anymore, TODO");
 })
 
-app.listen(PORT, function(){
-    console.log("We are always listening at " + PORT)
+app.get("/todos", function(req, res){
+    res.json(todos);
 })
+
+//GET individual todo
+
+app.get('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    // var todoId = parseInt(req.params.id, 10);
+    var matchedTodo;
+
+
+    todos.forEach(function (todo) {
+
+        if (todoId === todo.id) {
+            matchedTodo = todo;
+        }
+    });
+
+    if (matchedTodo) {
+        res.json(matchedTodo);
+    }else{
+        res.status(400).send();
+    }
+
+});
+
+
+app.listen(PORT, function(){
+    console.log("Port " + PORT +" is always listening")
+})
+
+

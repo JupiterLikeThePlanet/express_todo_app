a9a77d138ee954c7b49f51a3f6d5dd2e30e78b69
Jupiter <jupiterbaudot@gmail.com>
2018-01-18 16:40:30 -0800

    Add Put route

--- server.js
+++ server.js
@@ -22,6 +22,17 @@ var todoNextId = 1;
 //     completed: true,
 // }];
 
+// {
+//     "description": "Call Shelly",
+//     "completed": false
+// } {
+//     "description": "Eat Taco Bell Cravings Deal Package",
+//         "completed": false
+// } {
+//     "description": "Complain",
+//         "completed": true
+// }
+
 var todos = []
 
 app.use(bodyParser.json());
@@ -89,27 +100,27 @@ app.post("/todos", function(req, res){
 app.delete("/todos/:id", function(req, res){
     var todoId = parseInt(req.params.id, 10)
 
-    console.log("TODO id: " + todoId)
+    // console.log("TODO id: " + todoId)
 
     var deletedTodo = _.findWhere(todos, {id: todoId});
 
-    console.log("Deleted TODO: " + deletedTodo)
+    // console.log("Deleted TODO: " + deletedTodo)
 
     if (!deletedTodo) {
 
-        console.log("In the if statement");
+        // console.log("In the if statement");
 
         res.status(404).json({"error": "No TODO found with that id"});
 
 
     }else{
-        console.log("In the else statement")
+        // console.log("In the else statement")
 
         var todoArray = _.without(todos, deletedTodo);
 
-        console.log("Todo Array: " + todoArray)
+        // console.log("Todo Array: " + todoArray)
 
-        console.log("Deleted TODO: " + deletedTodo);
+        // console.log("Deleted TODO: " + deletedTodo);
 
         res.json(todoArray);
 
@@ -117,6 +128,58 @@ app.delete("/todos/:id", function(req, res){
 
 
 });
+
+
+// PUT
+
+app.put("/todos/:id", function(req, res){
+    var todoId = parseInt(req.params.id, 10)
+
+    console.log("TODOid: " + todoId);
+
+    var body = _.pick(req.body, 'description', 'completed')
+
+    console.log(body);
+
+    var updatedTodo = _.findWhere(todos, {id: todoId});
+
+    console.log("updated TODO: " + updatedTodo);
+
+    if (!updatedTodo) {
+
+        console.log("In Outer IF statement");
+        res.status(404).json({"error": "No TODO item with this id"})
+
+    } else {
+
+        console.log("In OUTER ELSE statement")
+
+        // var body = _.pick(req.body, 'description', 'completed')
+
+        // console.log("body: " + body);
+
+        if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+
+            console.log("In Outer IF statement");
+
+            return res.status(400).send();
+
+        }
+
+        updatedTodo.description = body.description.trim();
+        updatedTodo.completed = body.completed;
+        // updatedTodo.body.description = body.description.trim();
+        // updatedTodo.body.completed = body.completed;
+
+        console.log("Updates: " + updatedTodo.description + ", " + updatedTodo.completed )
+
+        console.log("UPDATED > description: " + body.description, ", completed: " + body.completed);
+
+        res.json(body);
+
+    }
+
+})
 
 
 

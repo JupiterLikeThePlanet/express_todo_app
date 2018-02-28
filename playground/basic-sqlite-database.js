734b73e820aad9f79fef9c039ad48484971dae93
Jupiter <jupiterbaudot@gmail.com>
2018-02-27 18:05:24 -0800

    Add initial validations tobject model

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -1,32 +1,40 @@
 var Sequelize = require ('sequelize');
 var sequelize = new Sequelize(undefined, undefined, undefined, {
     'dialect': 'sqlite',
-    'storage': __dirname + '/basic-sqlite-database'
+    'storage': __dirname + '/basic-sqlite-database.sqlite'
 });
 
 //defining object and attributes
 
 var Todo = sequelize.define('todo', {
     description: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            len: [1, 250]
+        }
     },
     completed: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
     }
 })
 
 
 
-    //force: true is a sync param that will drop tables
-sequelize.sync().then(function() {
+    //{force: true} is a sync param that will drop tables
+sequelize.sync({force: true}).then(function() {
     console.log('Everything is synced');
 
-    // Todo.create({
-    //     description: "Call Friends",
-    //     completed: false
-    // }).then(function(todo){
-    //     console.log('FINISHED!');
-    //     console.log(todo);
-    // });
+    Todo.create({
+        description: "Oingo Boingo",
+        completed: false
+    }).then(function(todo){
+        console.log('FINISHED!');
+        console.log(todo);
+    }).catch(function(e){
+        console.log("ERROR : " + e.message);
+    });
 });
 

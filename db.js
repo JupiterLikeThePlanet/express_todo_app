97d10d21f1711ca80ae58481ac4c1742d84dd7cd
Jupiter <jupiterbaudot@gmail.com>
2018-03-03 23:44:28 -0800

    Remove quotes for dialect in postgres part of if statement

--- db.js
+++ db.js
@@ -1,31 +1,66 @@
-var Sequelize = require ('sequelize');
+var Sequelize = require('sequelize');
 // will either load prod or dev, then upload approp db
 var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
-
 if (env === 'production') {
-
     sequelize = new Sequelize(process.env.DATABASE_URL, {
-        'dialect': 'postgres'
+        dialect: 'postgres'
     });
-
-}else{
-
+} else {
     sequelize = new Sequelize(undefined, undefined, undefined, {
         'dialect': 'sqlite',
         'storage': __dirname + '/data/dev-todo-api.sqlite'
     });
-
 }
 
+var db = {};
 
-
-var db = {}
-
-db.todo = sequelize.import(__dirname + '/models/todo.js')
-db.sequelize = sequelize
-db.Sequelize = Sequelize
+db.todo = sequelize.import(__dirname + '/models/todo.js');
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
 //setting it to an object allows up to pass multiple things instead of just one
 module.exports = db;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var Sequelize = require ('sequelize');
+// // will either load prod or dev, then upload approp db
+// var env = process.env.NODE_ENV || 'development';
+// var sequelize;
+//
+//
+// if (env === 'production') {
+//     sequelize = new Sequelize(process.env.DATABASE_URL, {
+//         'dialect': 'postgres'
+//     });
+// }else{
+//     sequelize = new Sequelize(undefined, undefined, undefined, {
+//         'dialect': 'sqlite',
+//         'storage': __dirname + '/data/dev-todo-api.sqlite'
+//     });
+// }
+//
+// var db = {}
+//
+// db.todo = sequelize.import(__dirname + '/models/todo.js')
+// db.sequelize = sequelize
+// db.Sequelize = Sequelize
+//
+// //setting it to an object allows up to pass multiple things instead of just one
+// module.exports = db;
